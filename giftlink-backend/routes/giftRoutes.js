a59898269e7368da8d7150a824bf98509e5e9df9
Leart Ramadani <leart.ramadani06gmail.com>
2026-09-12 15:16:20 +0200

    dsa

---
+++ giftlink-backend/routes/giftRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../db');
+
+// Get all gifts
+router.get('/', async (req, res, next) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gifts = await collection.find({}).toArray();
+        res.json(gifts);
+    } catch (e) {
+        next(e);
+    }
+});
+
+// Get a single gift by ID
+router.get('/:id', async (req, res, next) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const id = req.params.id;
+        const gift = await collection.findOne({ id: id });
+
+        if (!gift) {
+            return res.status(404).send("Gift not found");
+        }
+
+        res.json(gift);
+    } catch (e) {
+        next(e);
+    }
+});
+
+module.exports = router;
