794c989c619047d2e9b6152880ab2a064e266317
Leart Ramadani <leart.ramadani06gmail.com>
2026-09-12 15:14:29 +0200

    s

---
+++ giftlink-backend/db.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+
+// MongoDB connection URL with authentication
+const url = `${process.env.MONGO_URL}`;
+
+let dbInstance = null;
+const dbName = "giftdb";
+
+async function connectToDatabase() {
+    if (dbInstance) {
+        return dbInstance;
+    }
+
+    const client = new MongoClient(url);
+
+    // Task 1: Connect to MongoDB
+    await client.connect();
+
+    dbInstance = client.db(dbName);
+
+    return dbInstance;
+}
+
+module.exports = connectToDatabase;
